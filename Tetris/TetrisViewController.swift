3770484f7fd9172360603e55eaa291f3651f0c61
Invader Zim <pwstern@gmail.com>
2020-05-01 14:51:58 -0700

    Initial setup.

---
+++ Tetris/TetrisViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TetrisViewController.swift
+//  Tetris
+//
+//  Created by Phil Stern on 5/1/20.
+//  Copyright © 2020 Phil Stern. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+import SceneKit
+
+class TetrisViewController: UIViewController {
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
